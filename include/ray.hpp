c768f552a454723567b0dba48177427abf25f8be
seungwoo7050 <seungwoo7050@naver.com>
2025-07-19 11:34:14 +0900

    feat(camera): 화면 좌표를 카메라 광선으로 변환

--- include/ray.hpp
+++ include/ray.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "ray/camera.hpp"
 #include "ray/geometry.hpp"
 #include "ray/material.hpp"
 #include "ray/math.hpp"
---
+++ include/ray/camera.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "ray/scene.hpp"
+
+namespace ray {
+
+struct CameraFrame {
+    Vec3 forward;
+    Vec3 right;
+    Vec3 up;
+    double viewportWidth;
+    double viewportHeight;
+};
+
+CameraFrame buildCameraFrame(const Camera& camera, int width, int height);
+Ray makeCameraRay(const Camera& camera,
+                  int width,
+                  int height,
+                  double pixel_x,
+                  double pixel_y);
+
+}  // namespace ray
