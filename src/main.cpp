5cd6dac61bdc4d35ce9a59b629d18dcb8201d4a7
seungwoo7050 <seungwoo7050@naver.com>
2025-07-31 11:52:11 +0900

    refactor(cli): 위치 인자와 checksum option 모델 구성

--- src/main.cpp
+++ src/main.cpp
@@ -6,33 +6,64 @@
 
 namespace {
 
-void print_usage() {
-    std::cerr << "usage: ./ray-scene-tracer <scene.rt> <output.ppm> [--checksum]\n";
+struct CliOptions {
+    std::string scenePath;
+    std::string outputPath;
+    bool printChecksum = false;
+    ray::RenderSettings renderSettings;
+};
+
+void printUsage() {
+    std::cerr
+        << "usage: ray-scene-tracer <scene.rt> <output.ppm>"
+        << " [--checksum]\n";
+}
+
+bool parseCli(int argc, char** argv, CliOptions& options) {
+    if (argc < 3) {
+        return false;
+    }
+    options.scenePath = argv[1];
+    options.outputPath = argv[2];
+
+    bool seen_checksum = false;
+
+    for (int index = 3; index < argc; ++index) {
+        const std::string option = argv[index];
+        if (option == "--checksum") {
+            if (seen_checksum) {
+                return false;
+            }
+            seen_checksum = true;
+            options.printChecksum = true;
+            continue;
+        }
+        return false;
+    }
+    return true;
 }
 
 }  // namespace
 
 int main(int argc, char** argv) {
-    if (argc != 3 && argc != 4) {
-        print_usage();
+    CliOptions options;
+    if (!parseCli(argc, argv, options)) {
+        printUsage();
         return 2;
     }
-    const std::string scene_path = argv[1];
-    const std::string output_path = argv[2];
-    const bool print_checksum = argc == 4 && std::string(argv[3]) == "--checksum";
-    if (argc == 4 && !print_checksum) {
-        print_usage();
-        return 2;
-    }
+
     try {
-        const ray::Scene scene = ray::loadScene(scene_path);
-        const ray::Image image = ray::renderScene(scene);
-        ray::writePpm(image, output_path);
-        if (print_checksum) {
+        const ray::Scene scene =
+            ray::loadScene(options.scenePath);
+        const ray::Image image =
+            ray::renderScene(scene, options.renderSettings);
+        ray::writePpm(image, options.outputPath);
+        if (options.printChecksum) {
             std::cout << ray::checksumHex(image) << '\n';
         }
     } catch (const std::exception& error) {
-        std::cerr << "ray-scene-tracer: " << error.what() << '\n';
+        std::cerr << "ray-scene-tracer: "
+                  << error.what() << '\n';
         return 1;
     }
     return 0;
